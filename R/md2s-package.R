673b6e2ba32a6911d30f77c9af7c39502ee8c616
Peter Bachman <peter.j.bachman@gmail.com>
2022-05-09 15:00:36 -0500

    Update documentation

--- R/md2s-package.R
+++ R/md2s-package.R
@@ -1,6 +1,16 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
+#' Multi-Dataset Multidimensional Scaling
+#'
+#' The MD2S package computes a latent factor common to two matrices as well
+#' as an idiosyncratic factor unique to each. In addition, it offers a flexible
+#' modeling strategy that permits the scaled locations to be a function of
+#' covariates, and efficient implementation allows for inference through
+#' resampling
+#'
+#' @name md2s-package
+#' @aliases md2s-package
+#' @docType package
+#' @author Ted Enamorado \email{ted.enamorado@@gmail.com},
+#'
+#' Maintainer: Peter Bachman \email{bachman.p@wustl.edu}
+#' @keywords package
 NULL
