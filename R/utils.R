acb343e440e9f31c72d32d05723b4903c7572f42
Peter Bachman <peter.j.bachman@gmail.com>
2022-05-10 13:46:15 -0500

    purge unnecessary functions

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,6 @@
 # Purely internal functions, for use within md2s and md2sPermute
 
+# Used in md2sInner, md2s,
 ##standardize what's pased through, that's it.
 ## Give input
 my.norm <- function(x) {
@@ -11,6 +12,7 @@ my.norm <- function(x) {
   (x / sum(x^2)^.5)
 }
 
+# Only used in md2sInner
 ##Input optional covariates for explaining scaled locations in the shared subspace
 cleanup <- function(X.c) {
   #If dim of X.c is greater than 0, then...
@@ -62,14 +64,7 @@ check.cor.y <- function(z.run) {
   stats::var(wy)
 }
 
-## Checks the correlation and outputs it?
-## TODO Add inputs for x1 and y1 for my sanity
-check.cor <- function(z.run) {
-  wX <- as.vector(((X1) %*% t(X1)) %*% z.run)
-  wy <- as.vector(((y1) %*% t(y1)) %*% z.run)
-  stats::cov(wX, wy)
-}
-
+# Used in md2s
 # `make.int` standardizes the rows (columns) of a matrix such that each
 #   row (column) of the matrix has the same standard deviation & mean. I think
 #   it also does something with the grand mean.
@@ -95,58 +90,8 @@ fastres <- function(x, z) {
   res - make.int(res)
 }
 
+#
 ## Creates a sample matrix?
 sample.mat <- function(X) {
   apply(X, 2, FUN = function(x) sample(x, length(x), replace = FALSE))
-}
-
-## No clue yet
-tfidf <- function(mat) {
-  tf <- mat
-
-  ## show the number of columns that don't equal 0?
-  id <- function(col) {
-    sum(!col == 0)
-  }
-
-  idf <- log(nrow(mat) / apply(mat, 2, id))
-
-  tfidf <- mat
-
-  ## For every word
-  for (word in names(idf)) {
-    tfidf[, word] <- tf[, word] * idf[word]
-  }
-  return(tfidf)
-}
-
-## Normalizing function?
-# `my.norm` creates z-scores for every value of input vector. - Patrick
-#I'm not sure that it does. It's dividing by the square root of the sum of entries squared. It's just dividing by a common value that surpresses spread.
-#I think that if this were creating a z-value, then we would have to multiply by root(n) and divide by some sd parameter.
-my.norm <- function(x) {
-  x <- as.vector(x)
-  x <- x - mean(x)
-  (x / sum(x^2)^.5)
-}
-
-##  impute missing values by iteratively double-centering the matrix. All na values are set to 0.
-#  Note that the method choice for imputing values need not be of this method: Any method can be used.
-dubcent.impute <- function(X) {
-  X <- as.matrix(X)
-  miss.mat <- is.na(X)
-  X[is.na(X)] <- .5
-  for (i in 1:10000) {
-    Xlast <- X
-    X <- X - make.int(X)
-    X[miss.mat] <- 0
-    if (sum(abs(Xlast - X)) == 0) break
-  }
-  X
-}
-
-## No clue yet
-make.norm <- function(x) {
-  edf <- sapply(x, FUN = function(z) sum(x <= z)) / (length(x) + 1)
-  stats::qnorm(edf)
 }
